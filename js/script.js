feb85bd8e25af9c5d240446e0d4d7982be2d3023
Evseredyn <zhenyavn@gmail.com>
2024-05-27 12:37:51 +0300

    add formsending functions

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
         formData.append('image', formImage.files[0]);
 
         if (error === 0) {
+            form.classList.add('_sending');
             let response = await fetch('mail_raschet.php', {
                 method: 'POST',
                 body: formData
@@ -20,9 +21,17 @@ document.addEventListener('DOMContentLoaded', function () {
             if (response.ok) {
                 let result = await response.json();
                 alert(result.message);
+                formPreview.innerHTML = '';
                 form.reset();
+                form.classList.remove('_sending');
+            } else {
+              alert('Помилка');
+              form.classList.remove('_sending');
             }
+        } else {
+            alert('Заповніть обов\'язкові поля!'); 
         }
+
     }
 
 
@@ -43,21 +52,24 @@ document.addEventListener('DOMContentLoaded', function () {
        }
        return error;
     }
-    function formAddError(input) {
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-    }
-    function formRemoveError(input) {
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-    }
-    //Функция теста email
-    //function emailTest(input) {
-    //    return !/\w+([\.-?\w+)*@\w+([\/.-]?\w+)(\.\w{2,8})+$/.test(input.value);
+    //Надання/забирання класу у батьків
+    //function formAddError(input) {
+    //    input.parentElement.classList.add('_error');
+    //    input.classList.add('_error');
     //}
+    //function formRemoveError(input) {
+    //    input.parentElement.classList.add('_error');
+    //    input.classList.add('_error');
+    //}
+    //Функция теста email
+    function emailTest(input) {
+        return !/\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
 
     //Получаем инпут файл в переменную
     const formImage = document.getElementById('formImage');
+    //Получаем див для превью в переменную
+    const formPreview = document.getElementById('formPreview');
 
     //Слушаем изменения в инпут файле
     formImage.addEventListener('change', () => {
@@ -66,15 +78,24 @@ document.addEventListener('DOMContentLoaded', function () {
     
     function uploadFile(file) {
         //проверяем тип файла
-        if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+        if (!['image/jpeg', 'image/png', 'image/pdf'].includes(file.type)) {
             alert('Дозволені тільки зображення.');
             formImage.value = '';
             return;
         }
         //проверяем размер файла
-        if (file.size > 8 * 1024 * 1024) {
+        if (file.size > 1 * 1024 * 1024) {
             alert('Файл має бути менше 8 МБ.');
             return;
         }
+
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            formPreview.innerHTML = '<img src="${e.target.result}" alt="Фото">';
+        };
+        reader.onerror = function(e) {
+            alert('Помилка');
+        };
+        reader.readAsDataURL(file);
     }
 });

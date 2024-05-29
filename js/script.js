0d20a31ee9b87c07ef296cec30dbd992eda16988
Evseredyn <zhenyavn@gmail.com>
2024-05-29 08:45:00 +0300

    to prepare form

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
 "use strict"
 
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
+    const formIds = ['demoForm4', 'demoForm5', 'demoForm'];
 
-    async function formSend(e) {
+    formIds.forEach(formId => {
+        const form = document.getElementById(formId);
+
+        form.addEventListener('submit', e => formSend(e, form));
+    });
+
+    async function formSend(e, form) {
         e.preventDefault();
+        const activeFormId = form.getAttribute('id');
 
         let error = formValidate(form);
 
+        let formImage = form.querySelector('input[type="file"]');
+
         let formData = new FormData(form);
         formData.append('image', formImage.files[0]);
+        formData.append('formId', activeFormId)
 
         if (error === 0) {
             form.classList.add('_sending');
-            let response = await fetch('mail_raschet.php', {
+            let response = await fetch('phpmailer/src/mail_raschet.php', {
                 method: 'POST',
                 body: formData
             });
+
             if (response.ok) {
                 let result = await response.json();
                 alert(result.message);
@@ -25,77 +35,106 @@ document.addEventListener('DOMContentLoaded', function () {
                 form.reset();
                 form.classList.remove('_sending');
             } else {
-              alert('Помилка');
-              form.classList.remove('_sending');
+                alert('Помилка');
+                form.classList.remove('_sending');
             }
         } else {
-            alert('Заповніть обов\'язкові поля!'); 
+            alert('Заповніть обов\'язкові поля!');
         }
-
     }
-
 
     function formValidate(form) {
         let error = 0;
-        let formReq = document.querySelectorAll('._req');
+        const formId = form.getAttribute('id');
+        let formReq = document.querySelectorAll('#' + formId + '>ul>li>label>._req');
 
-        for (let index = 0; index  < formReq.length; index++) {
+        for (let index = 0; index < formReq.length; index++) {
             const input = formReq[index];
             formRemoveError(input);
-        
-        if (input.classList.contains('_email')){
-            if  (emailTest(input)) {
-                formAddError(input);
-                error++;
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
             }
         }
-       }
-       return error;
+
+        return error;
     }
-    //Надання/забирання класу у батьків
-    //function formAddError(input) {
-    //    input.parentElement.classList.add('_error');
-    //    input.classList.add('_error');
-    //}
-    //function formRemoveError(input) {
-    //    input.parentElement.classList.add('_error');
-    //    input.classList.add('_error');
-    //}
+
+    // Надання/забирання класу у батьків
+    function formAddError(input) {
+       input.parentElement.classList.add('_error');
+       input.classList.add('_error');
+    }
+
+    function formRemoveError(input) {
+       input.parentElement.classList.add('_error');
+       input.classList.add('_error');
+    }
+
     //Функция теста email
     function emailTest(input) {
-        return !/\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
     }
 
     //Получаем инпут файл в переменную
     const formImage = document.getElementById('formImage');
+    const formImage4 = document.getElementById('formImage4');
+    const formImage5 = document.getElementById('formImage5');
+
     //Получаем див для превью в переменную
     const formPreview = document.getElementById('formPreview');
 
     //Слушаем изменения в инпут файле
     formImage.addEventListener('change', () => {
-        uploadFile(formImage.files[0]);
+        uploadFile(formImage.files[0], formImage.id);
     });
-    
-    function uploadFile(file) {
+
+    formImage4.addEventListener('change', () => {
+        uploadFile(formImage4.files[0], formImage4.id);
+    });
+
+    formImage5.addEventListener('change', () => {
+        uploadFile(formImage5.files[0], formImage5.id);
+    });
+
+    function uploadFile(file, inputId) {
         //проверяем тип файла
-        if (!['image/jpeg', 'image/png', 'image/pdf'].includes(file.type)) {
-            alert('Дозволені тільки зображення.');
+        const mimes = ['image/jpeg', 'image/png', 'application/pdf', 'image/webp'];
+
+        if (!mimes.includes(file.type)) {
+            const allowedMimes = mimes.map((value) => value.split('/').pop()).join(', ');
+            alert('Дозволені тільки такі формати: ' + allowedMimes);
             formImage.value = '';
             return;
         }
         //проверяем размер файла
-        if (file.size > 1 * 1024 * 1024) {
-            alert('Файл має бути менше 8 МБ.');
+        const MB = 8;
+
+        if (file.size > MB * 1024 * 1024) {
+            formImage.value = '';
+            alert('Файл має бути меньше '+ MB +' МБ.');
             return;
         }
 
         var reader = new FileReader();
-        reader.onload = function (e) {
-            formPreview.innerHTML = '<img src="${e.target.result}" alt="Фото">';
-        };
+        if (file.type !== 'application/pdf' && inputId === 'formImage') {
+            reader.onload = function (e) {
+                formPreview.innerHTML = `<img src="${ e.target.result }" alt="Фото">`;
+            };
+        }
+
         reader.onerror = function(e) {
             alert('Помилка');
         };
+
         reader.readAsDataURL(file);
     }
 });
--- phpmailer/src/mail_raschet.php
+++ phpmailer/src/mail_raschet.php
@@ -1,28 +1,36 @@
 <?php
-   /*use PHPMailer\PHPMailer\PHPMailer;
-   use PHPMailer\PHPMailer\Exception;
-   
-   require 'phpmailer/scr/Exception.php';
-   require 'phpmailer/scr/PHPMailer.php';*/
-
-   require_once('PHPMailer.php');
-   require_once('Exception.php');
    require_once('SMTP.php');
+   require_once('Exception.php');
+   require_once('PHPMailer.php');
 
    use PHPMailer\PHPMailer\PHPMailer;
-   use PHPMailer\PHPMailer\Exception;
 
-   /*$mail = new PHPMailer(true);*/
+   if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+       header('HTTP/1.0 404 Not Found');
+       exit();
+   }
+
+   $mail = new PHPMailer();
+   $mail->isSMTP();
+   $mail->Host = 'sandbox.smtp.mailtrap.io';
+   $mail->SMTPAuth = true;
+   $mail->Port = 2525;
+   $mail->Username = '35159d57085c8e';
+   $mail->Password = 'ca07976004eb60';
+
+//   $mail = new PHPMailer(true);
    $mail->CharSet = 'UTF-8';
    $mail->setLanguage('uk', 'phpmailer/language/');
    $mail->IsHTML();
 
    //От кого письмо
-   $mail->setFrom('info@docukraine.com.ua', 'Сайт ДОКУМЕНТИ УКРАЇНА'); 
-   //Кому отправить
+
+   $mail->setFrom('info@docukraine.com.ua', 'Сайт ДОКУМЕНТИ УКРАЇНА');
+
+//Кому отправить
    $mail->addAddress('info@docukraine.com.ua');
-   $mail->addAddress('zhenyavn@gmail.com');
-   //Тема письма
+   $mail->addAddress('test@test.loc');
+//Тема письма
    $mail->Subject = 'Сайт ДОКУМЕНТИ УКРАЇНА';
 
    //Тело письма
@@ -40,30 +48,40 @@
    if(trim(!empty($_POST['message']))){
     $body.='<p><strong>Повідомлення:</strong> '.$_POST['message'].'</p>';
    }
+   if(trim(!empty($_POST['formId']))){
+       $body.='<p><strong>ID форми:</strong> '.$_POST['formId'].'</p>';
+   }
 
    //Прикрепить файл
-   if (!empty($_FILES['image']['tmp_name'])) {
-    //путь загрузки файла
-    $filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
-    //грузим файл
-    if (copy($_FILES['image']['tmp_name'], $filePath)){
-        $fileAttach = $filePath;
-        $body.='<p><strong>Фото у додатку</strong>';
-        $mail->addAttachment($fileAttach);
+    if (!empty($_FILES['image']['tmp_name'])) {
+        // Путь для збереження файлу
+        $file = $_FILES['image']['name'];
+        $originalName = pathinfo($file, PATHINFO_FILENAME);
+        $ext = pathinfo($file, PATHINFO_EXTENSION);
+
+        // Формуємо нову назву файлу
+        $newFileName = $originalName . time() . '.' . $ext;
+
+        $filePath = __DIR__ . "\\..\\..\\files\\" . $newFileName;
+
+        // Завантажуємо файл
+        if (copy($_FILES['image']['tmp_name'], $filePath)) {
+            $fileAttach = $filePath;
+            $body .= '<p><strong>Фото у додатку</strong>';
+            $mail->addAttachment($fileAttach);
+        }
     }
-   }
 
    $mail->Body = $body;
 
    //отправляем
    if (!$mail->send()) {
-    $message = 'Помилка';
+       $message = 'Помилка';
    } else {
-    $message = 'Форма надіслана!';
+       $message = 'Форма надіслана!';
    }
 
    $response = ['message' => $message];
 
    header('Content-type: application/json');
    echo json_encode($response);
-?>
